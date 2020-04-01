8f6cf1d0fce959d468b8cf29ce4b66bc632e3ae7
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-04-01 15:08:08 +0900

    Update hello.cpp
    revise hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main(){
 
-    cout << "hello world!" << endl;
+
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += i+1;
+    }
+    cout << sum << endl;
 
     return 0;
 }
